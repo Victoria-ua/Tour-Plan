19ff07226ce4a37cc7184d30d28129cfc71e7b96
Victoria-ua <nechaieva.victoria@gmail.com>
2021-02-28 00:20:22 +0300

    Apdated: Validation for Modal Windows

--- Js/main.js
+++ Js/main.js
@@ -49,4 +49,24 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+  //обработка форм
+  $('.form').each(function () {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+       required: "Please specify your name",
+       minlength: "Should be not less then 2 characters",
+      },
+      email: {
+       required: "We need your email address to contact you",
+       email: "Your email address must be in the format of name@domain.com",
+      },
+      number: {
+       required: "write your phone number, bi*ch!",
+      },        
+    },
+  });
+  })
 })
+
